fc506dbd377434ee3fb33a04864373eb94211a8f
pastre <pastre68@gmail.com>
2021-02-22 10:42:55 -0300

    Integrated mongo

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 import discord
 from discord.ext import commands
-
-bot = commands.Bot(command_prefix="sg!")
+import pymongo 
 
 suggestions_channel_id = None
+bot = commands.Bot(command_prefix="sg!")
+client = pymongo.MongoClient("mongodb+srv://pastre:asdqwe123@cluster0.aarfo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.Suggestions
+suggestions = db["suggestions"]
+
+def add_suggestion(message, author):
+	print(f"add_suggestion! {message}, {author}")
+	suggestion = { "Autor": author, "Mensagem": message }
+	x = suggestions.insert_one(suggestion)
+
 
 @bot.command()
 async def ping(ctx, arg):
@@ -13,27 +22,28 @@ async def ping(ctx, arg):
 
 @bot.event
 async def on_message(message):
-	print(f"RECVD MESSAGE! {message}")
 	if (message.guild is not None) or (message.author == bot.user):
 		await bot.process_commands(message)
 		return
 	if suggestions_channel_id == None: 
 		await message.channel.send("Ops! Temos um problema!")
 		return
-	author = message.author
+	author = message.author.name
 	content = message.content
 	await forward_suggestion(content, author)
 
 @bot.command()
 async def set_channel(ctx):
-	print("COMANDO!!!!!")
 	global suggestions_channel_id 
 	suggestions_channel_id = ctx.channel.id
 	await ctx.channel.send(f"Channel configured on {ctx.channel.id}!")
 
 async def forward_suggestion(message, author):
+	print(f"forward_suggestion! {message}, {author}")
 	if suggestions_channel_id == None: return
 	channel = bot.get_channel(suggestions_channel_id)
-	await channel.send(f"{author} - {message}")
+	add_suggestion(message, author)
+	await channel.send(f"*{message}*")
 
 bot.run('ODEzMzg1MTM3OTg2MzM4ODQ2.YDOh8A.ppyzMuQ0rqxjUUsNSL5RNJ0RIjk')
+
